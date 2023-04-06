5e744eab63b238c88351a0e1e0d6b22d2e3c49bd
justo340 <justusnjogu16@gmail.com>
2023-04-06 14:15:17 -0700

    static web page

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 13 14:21:54 2020
-@author: Robinson Montes
+@author: justus njogu
 """
 from fabric.api import local, put, run, env
 from datetime import datetime
 
 env.user = 'ubuntu'
-env.hosts = ['35.227.35.75', '100.24.37.33']
+env.hosts = ['34.224.17.222', '54.90.30.17']
 
 
 def do_pack():
